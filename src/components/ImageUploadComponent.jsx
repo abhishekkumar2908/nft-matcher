2ea52bdf6529739d1c41fc0c426fba9bc2c9c334
Gaurang <gaurangpatel.ahm@gmail.com>
2023-06-12 17:36:14 +0530

    fixed bug issue, even before uploading image it was showing No similar match

--- src/components/ImageUploadComponent.jsx
+++ src/components/ImageUploadComponent.jsx
@@ -9,7 +9,9 @@ function ImageUploadComponent() {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState('');
   const [similarNFTs, setSimilarNFTs] = useState([]);
-  const [assetInfo, setAssetInfo] = useState([]);
+  const [uploadState, setUploadState] = useState(false);
+
+  
   
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
@@ -27,51 +29,61 @@ function ImageUploadComponent() {
       .post('/find/any/similar/image', formData)
       .then((response) => {
         console.log("res", response.data);
-        setSimilarNFTs(response.data);               
+        setSimilarNFTs(response.data);  
+        setUploadState(true);
+
       })
       .catch((error) => {
         console.error(error);
       });
+
   };
   useEffect(() => {
-    const fetchAssetInfo = async () => {
-
-        const fetchedAssetInfo = [];
-        let i=0
-        console.log("similarNFTs.length",similarNFTs.length)
-        const url = similarNFTs[i].tokenMetadata;
-        console.log("url:", url);
-        while(i<similarNFTs.length) {
-
-          let response;
-          try {          
-           
-            if(url.match(/ipfs\/([a-zA-Z0-9]+)/)){
-              const ipfsHash = url.match(/ipfs\/([a-zA-Z0-9]+)/);
-              response = await axios.get(`https://ipfs.io/ipfs/${ipfsHash[1]}`);
-              console.log("ipfsHash response: ", response);
-            }    
-            else {
-            response = await axios.get(`https://testnets-api.opensea.io/api/v1/asset/${similarNFTs[i].address}/${similarNFTs[i].tokenId}/`);
-              if (response) {
-                console.log("log", response);
-                fetchedAssetInfo.push(response.data);
-                setAssetInfo(fetchedAssetInfo);                
-              }
-            }  
-            i++
-            } catch (error) {
-              console.error(error);
-            }
-            await new Promise((resolve) => setTimeout(resolve, 450));
-          }
- 
-    };
-
-    if(similarNFTs && similarNFTs.length) {
-        fetchAssetInfo();
+    if (fileName !== '') {
+      setUploadState(false);
     }
-  }, [similarNFTs]);
+  }, [fileName]);
+  // useEffect(() => {
+  //   const fetchAssetInfo = async () => {
+
+  //       const fetchedAssetInfo = [];
+  //       let i=0
+  //       console.log("similarNFTs.length",similarNFTs.length)
+  //       const url = similarNFTs[i].tokenMetadata;
+  //       console.log("url:", url);
+  //       while(i<similarNFTs.length) {
+
+  //         let response;
+  //         try {          
+           
+  //           if(url.match(/ipfs\/([a-zA-Z0-9]+)/)){
+  //             const ipfsHash = url.match(/ipfs\/([a-zA-Z0-9]+)/);
+  //             response = await axios.get(`https://ipfs.io/ipfs/${ipfsHash[1]}/metadata.json}`, { responseType: 'json' });
+  //             const metadata = response.data;
+
+  //             console.log("ipfsHash response: ", metadata);
+  //           }    
+  //           else {
+  //           response = await axios.get(`https://testnets-api.opensea.io/api/v1/asset/${similarNFTs[i].address}/${similarNFTs[i].tokenId}/`);
+  //             if (response) {
+  //               console.log("log", response);
+  //               fetchedAssetInfo.push(response.data);
+  //               setAssetInfo(fetchedAssetInfo);                
+  //             }
+  //           }  
+  //           i++
+  //           } catch (error) {
+  //             console.error(error);
+  //           }
+  //           await new Promise((resolve) => setTimeout(resolve, 450));
+  //         }
+ 
+  //   };
+
+  //   if(similarNFTs && similarNFTs.length) {
+  //       fetchAssetInfo();
+  //   }
+  // }, [similarNFTs]);
 
   
 
@@ -90,36 +102,26 @@ function ImageUploadComponent() {
         </button>
       </form>
       <div style={{marginTop: '18px'}}>
-        { similarNFTs.length > 0 ? (
+        { uploadState && similarNFTs.length > 0 ? (
           <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px' }}>
           {similarNFTs.map((nft) => (
             <div key={nft.id} style={{ width: '300px', borderRadius: '8px', overflow: 'hidden', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)' }}>
+              
               <div style={{ position: 'relative' }}>
                 <ImageContainer imageUrl={nft.imageOriginalUrl} style={{ width: '100%', height: 'auto', borderRadius: '8px 8px 0 0' }} />
-                {/* <div style={{ position: 'absolute', top: '10px', right: '10px', backgroundColor: 'rgba(0, 0, 0, 0.8)', color: '#fff', padding: '4px 8px', borderRadius: '4px', fontSize: '12px', fontWeight: 'bold' }}>
-                {nft.nftId}
-                </div> */}
               </div>
               <div style={{ padding: '10px', backgroundColor: '#fff', maxHeight: '120px', overflowY: 'auto' }}>
                 <p style={{ fontSize: '16px', fontWeight: 'bold', margin: '0 0 8px' }}>NFT ID: {nft.nftId}</p>
                 <p style={{ fontSize: '14px', margin: '0 0 8px' }}>Token ID: {nft.tokenId}</p>
-                <p style={{ fontSize: '14px', margin: '0', overflowWrap: 'break-word' }}>
-                  Address: {nft.address}
-                </p>
+                <p style={{ fontSize: '14px', margin: '0', overflowWrap: 'break-word' }}>Address: {nft.address}</p>
               </div>
 
             </div>
           ))}
-        </div>
-        
-        
-        
-        
-          
-        
+        </div>  
+               
         ) : (
-          <p>No similar NFTs found.</p>
-        )}
+          uploadState && <p>No similar NFTs found.</p>        )}
       </div>
     </div>
   );
